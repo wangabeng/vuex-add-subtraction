ec7327295f38b31679195e8d53a859bbc453c1f6
ben <wangabeng@163.com>
2017-05-06 15:28:28 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="app">
+    <h3>welcome vuex demo</h3>
+    <input type='button' value='增加' @click='increment'>
+    <input type='button' value='减少' @click='decrement'>
+    <input type='button' value='偶数才增加' @click='addOdd'>
+    <input type='button' value='异步增加' @click='addAsync'>
+    <p>现在数字为：{{count2}}</p>
+    <p>{{getOdd}}</p>
+  </div>
+</template>
+
+<script>
+import {mapGetters,mapActions} from 'vuex'
+export default {
+  computed:mapGetters([
+    'count2',//是个函数,
+    'getOdd'
+  ]),
+  methods:mapActions([
+    'increment',
+    'decrement',
+    'addOdd',
+    'addAsync'
+  ])
+
+}
+</script>
+
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/store.js
@@ -0,0 +1,57 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex);
+
+const state={
+	count:10
+};
+const actions={
+	increment:function({commit}){ //第一步 直接执行此函数 
+		commit('increment2'); //第二步 触发increment2函数 
+	},
+	decrement:function({commit}){ 
+		commit('decrement2'); 
+	},
+	addOdd:function({commit,state}){ 
+		if (state.count%2==0){
+			commit('increment2'); 
+		}
+	},
+	addAsync:function({commit}){ 
+		new Promise(
+			function(resolve){
+				setTimeout(function(){
+					commit('increment2');
+					resolve();
+				},1000);
+			}
+		);
+	}
+};
+
+const mutations={
+	increment2:function(state){
+		//alert(1);
+		return state.count++;
+	},
+	decrement2:function(state){
+		//alert(1);
+		return state.count--;
+	}
+}
+
+const getters={
+	count2(state){
+		return state.count;
+	},
+	getOdd(state){
+		return state.count%2==0?'结果是偶数':'结果是奇数';
+	}
+}
+
+export default new Vuex.Store({
+	state,
+	mutations,
+	actions,
+	getters
+})
